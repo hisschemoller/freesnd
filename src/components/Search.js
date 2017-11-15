06bc0f67a347c089bec7429286895a69e7986a68
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2017-11-15 14:33:33 +0100

    Search form controlled componenets.

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,12 +2,32 @@ import React, { Component } from 'react';
 
 class Search extends Component {
     
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleChange(e) {
+        this.setState({
+            value: e.target.value
+        });
+    }
+    
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log('submit');
+    }
+    
     render() {
         return (
-            <div className="search">
-                <input type="text" />
+            <form className="search" onSubmit={this.handleSubmit}>
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
                 <input type="submit" />
-            </div>
+            </form>
         );
     }
 }
