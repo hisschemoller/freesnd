82413cf2a816350381c714a38f54c932d494d6b2
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2017-11-28 17:55:35 +0100

    Result preview play button.

--- src/components/Results/Result.js
+++ src/components/Results/Result.js
@@ -6,6 +6,7 @@ class Result extends Component {
         return (
             <li className={s.listItem}>
                 <img src={this.props.img} alt={this.props.name} className={s.waveform}/>
+                <button>p</button>
                 <span className={s.name}>{this.props.name}</span>
             </li>);
     }
