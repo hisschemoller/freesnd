3e8ba2334e606e2bd73c94e52fd1767b11241b27
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2017-12-06 18:20:05 +0100

    Pagination buttons for previous and next series of pages.

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -13,16 +13,50 @@ class Pagination extends Component {
     }
     
     render() {
-        const numPages = Math.ceil(this.props.count / this.props.pageSize);
+        const numButtons = 4,
+            numPages = Math.ceil(this.props.count / this.props.pageSize),
+            pagesBefore = [],
+            pagesAfter = [];
+        
+        let firstPageButton = '',
+            lastPageButton = '',
+            separatorBefore = '',
+            separatorAfter = '';
+        
+        if (this.props.page > numButtons + 1) {
+            firstPageButton = <button type="button" onClick={() => this.gotoPage(1)} disabled={numPages === 0}>1</button>;
+        }
+        
+        if (this.props.page < numPages - numButtons - 1) {
+            lastPageButton = <button type="button" onClick={() => this.gotoPage(numPages)} disabled={numPages === 0}>{numPages}</button>;
+        }
+        
+        if (this.props.page > numButtons + 2) {
+            separatorBefore = <span>...</span>;
+        }
+        
+        if (this.props.page < numPages - numButtons - 2) {
+            separatorAfter = <span>...</span>;
+        }
+        
+        for (let i = Math.max(1, this.props.page - numButtons), n = this.props.page; i < n; i++) {
+            pagesBefore.push(<button type="button" onClick={() => this.gotoPage(i)} disabled={numPages === 0} key={i}>{i}</button>);
+        }
+        
+        for (let i = Math.min(i + 1, numPages), n = Math.min(i + 1 + numButtons, numPages); i < n; i++) {
+            pagesAfter.push(<button type="button" onClick={() => this.gotoPage(i)} disabled={numPages === 0} key={i}>{i}</button>);
+        }
         
         return (
             <div className={s.pagination}>
                 <button type="button" onClick={() => this.gotoPage(this.props.page - 1)} disabled={this.props.page <= 1}>&lt;</button>
-                <button type="button" onClick={() => this.gotoPage(1)} disabled={numPages === 0}>1</button>
-                <span>...</span>
+                {firstPageButton}
+                {separatorBefore}
+                {pagesBefore}
                 <span>{this.props.page}</span>
-                <span>...</span>
-                <button type="button" onClick={() => this.gotoPage(numPages)} disabled={numPages === 0}>{numPages}</button>
+                {pagesAfter}
+                {separatorAfter}
+                {lastPageButton}
                 <button type="button" onClick={() => this.gotoPage(this.props.page + 1)} disabled={this.props.page === numPages}>&gt;</button>
             </div>
         );
