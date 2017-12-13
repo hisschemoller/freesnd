53a46f1fe3caef1b584bd6a4e651a61d4afe3a54
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2017-12-13 11:15:33 +0100

    Sound detail component toggles with button click.

--- src/components/Results/Result.js
+++ src/components/Results/Result.js
@@ -1,28 +1,46 @@
 import React, { Component } from 'react';
 import s from './Result.css';
+import ResultDetail from './ResultDetail';
 
 class Result extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            showDetails: false
+        };
+    }
+    
+    toggleDetail = (e) => {
+        this.setState({
+            showDetails: !this.state.showDetails
+        });
+    }
 
     render() {
         const classNames = `${s.listitem} ${this.props.active ? s['listitem--active'] : ''}`;
         return (
             <li className={classNames}>
-                <div className={s.waveform}>
-                    <img src={this.props.img} alt={this.props.name} />
-                </div>
-                <button onMouseDown={() => this.props.onPreviewButtonDown(this.props.index)} onMouseUp={() => this.props.onPreviewButtonUp()}>p</button>
-                <div className={s.info}>
-                    <div>
-                        <span className={s.name}>{this.props.name}</span>
-                        <button className={s.username} onClick={() => this.props.onUserOrTagClick(this.props.username)}>{this.props.username}</button>
-                        <span>{this.props.duration.toFixed(2)}, {this.props.rating.toFixed(1)}, {this.props.downloads}, {this.props.created}</span>
+                <div className={s.row}>
+                    <div className={s.waveform}>
+                        <img src={this.props.img} alt={this.props.name} />
                     </div>
-                    <div className={s.tags}>
-                        {this.props.tags.map((tag, i) => (
-                            <button key={tag} className={s.tag} onClick={() => this.props.onUserOrTagClick(tag)}>{tag}</button>
-                        ))}
+                    <button onMouseDown={() => this.props.onPreviewButtonDown(this.props.index)} onMouseUp={() => this.props.onPreviewButtonUp()}>p</button>
+                    <div className={s.info}>
+                        <div>
+                            <span className={s.name}>{this.props.name}</span>
+                            <button className={s.username} onClick={() => this.props.onUserOrTagClick(this.props.username)}>{this.props.username}</button>
+                            <span>{this.props.duration.toFixed(2)}, {this.props.rating.toFixed(1)}, {this.props.downloads}, {this.props.created}</span>
+                        </div>
+                        <div className={s.tags}>
+                            {this.props.tags.map((tag, i) => (
+                                <button key={tag} className={s.tag} onClick={() => this.props.onUserOrTagClick(tag)}>{tag}</button>
+                            ))}
+                        </div>
                     </div>
+                    <button type="button" onClick={this.toggleDetail}>d</button>
                 </div>
+                { this.state.showDetails ? <ResultDetail /> : null }
             </li>);
     }
 }
---
+++ src/components/Results/ResultDetail.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import s from './ResultDetail.css';
+
+class ResultDetail extends Component {
+    
+    render() {
+        return ( <div className={s.root}>ResultDetail</div> );
+    }
+}
+
+export default ResultDetail;
