7118840413dafc0941e50506c4f945889cd7b442
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2018-01-11 15:10:01 +0100

    Redux-persist added.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { applyMiddleware, compose, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
 import thunkMiddleware from 'redux-thunk';
+import { persistStore, persistCombineReducers } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/es/integration/react'
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import App from './components/App';
 import audioReducer from './reducers/audioReducer';
 import favsReducer from './reducers/favsReducer';
 import searchReducer from './reducers/searchReducer';
-
-const reducer = combineReducers({
-    audioState: audioReducer,
-    favsState: favsReducer,
-	searchState: searchReducer
-});
 
 const loggerMiddleware = createLogger({
     collapsed: true
@@ -26,17 +23,33 @@ const api = {
     token: '97fXJpalkrThSLwam15I5FZBSqYOHvk3DUbwCj65'
 }
 
-const store = createStore(
-    reducer,
+const configureStore = compose(
     applyMiddleware(
         thunkMiddleware.withExtraArgument(api),
-        loggerMiddleware
-    ));
+        loggerMiddleware),
+)(createStore);
+
+const config = {
+    key: 'root',
+    storage,
+}
+
+const combinedReducer = persistCombineReducers(config, {
+    audioState: audioReducer,
+    favsState: favsReducer,
+    searchState: searchReducer
+});
+
+const store = configureStore(combinedReducer);
+
+const persistor = persistStore(store);
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
-    </Provider>, 
+        <PersistGate persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>,
     document.getElementById('root')
 );
 
