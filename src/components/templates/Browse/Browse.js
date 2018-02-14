9c0783c79c57d787851842a93aadb9b1ef0d0623
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2018-02-14 10:51:48 +0100

    Template Browse added with the the original app content.

--- src/components/templates/Browse/Browse.js
+++ src/components/templates/Browse/Browse.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import Grid from '../../atoms/Grid/Grid';
 import Favourites from '../../organisms/favourites/Favourites';
 import History from '../../organisms/history/History';
 import Pagination from '../../organisms/pagination/Pagination';
 import Results from '../../organisms/resultslist/ResultsList';
 import Search from '../../organisms/search/Search';
 import WebAudio from '../../WebAudio';
+import s from './Browse.css';
 
 class Browse extends React.Component {
     
     render() {
         // return (<span>App goes here</span>);
         return (
-            <div className="app">
+            <Grid className={s.root}>
                 <WebAudio />
                 <div className="lib">
                     <Favourites />
@@ -22,7 +24,7 @@ class Browse extends React.Component {
                     <Pagination />
                     <History />
                 </div>
-            </div>
+            </Grid>
         );
     }
 }
