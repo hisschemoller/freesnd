2a736d2589f13c237497b397cc980bef5a72d315
Wouter Hisschemöller <wouter.hisschemoller@digitaslbi.com>
2018-02-14 10:52:40 +0100

    Layout component shows content based on route.

--- src/components/base/Layout/Layout.js
+++ src/components/base/Layout/Layout.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import s from './Layout.css';
 
 class Layout extends React.Component {
     render() {
-        return (<div className="layout">{this.props.children}</div>);
+        return (
+            <div className={s.layout}>
+                {this.props.children}
+            </div>
+        );
     }
 }
 
